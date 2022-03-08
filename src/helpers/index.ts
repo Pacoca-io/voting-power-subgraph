449a650c767d7ad75b611ec9f5c6920951e2e6c7
AmendoBobo <amendobobo404@protonmail.com>
2022-03-08 16:33:34 -0300

    Fix push to all users array

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, BigDecimal, store } from "@graphprotocol/graph-ts";
+import { Address, BigInt, BigDecimal, store, log } from "@graphprotocol/graph-ts";
 import { VotingPower } from "../../generated/VotingPower/VotingPower";
 import { User, AllUsers } from "../../generated/schema";
 
@@ -16,10 +16,6 @@ const ADDRESS_BLACKLIST = [
   toAddress("0xd0ceeacfee90aa1371e7ebb8df7ca9efb77d091d"), // TIMELOCK
 ];
 
-export function toDecimal(units: BigInt): BigDecimal {
-  return units.toBigDecimal().div(BD_18);
-}
-
 export default function setUser(address: string, balance: BigDecimal): void {
   let user = User.load(address);
 
@@ -36,24 +32,13 @@ export default function setUser(address: string, balance: BigDecimal): void {
   }
 }
 
-function pushUser(address: string): void {
-  let users = AllUsers.load(USERS_ID);
-
-  if (!users) {
-    users = new AllUsers(USERS_ID);
-  }
-
-  users.addresses.push(address);
-
-  users.save();
-}
-
 export function updateAllUsers(votingPower: VotingPower): void {
   const users = AllUsers.load(USERS_ID);
 
   if (!users) return;
 
   for (let i = 0; i < users.addresses.length; i++) {
+
     const address = toAddress(users.addresses[i]);
 
     if (User.load(address.toHexString())) {
@@ -62,10 +47,32 @@ export function updateAllUsers(votingPower: VotingPower): void {
   }
 }
 
+function pushUser(address: string): void {
+  let users = AllUsers.load(USERS_ID);
+
+  if (!users) {
+    users = new AllUsers(USERS_ID);
+  }
+
+  let addresses = users.addresses;
+  addresses.push(address);
+  users.addresses = addresses;
+  users.save();
+
+  log.debug(
+    'New user created: | Address: {} | Users Lenght: {}',
+    [address, `${users.addresses.length}`]
+  );
+}
+
 export function checkAddressBlackList(address: Address): boolean {
   return ADDRESS_BLACKLIST.includes(address);
 }
 
 export function toAddress(address: string): Address {
   return Address.fromString(address);
+}
+
+export function toDecimal(units: BigInt): BigDecimal {
+  return units.toBigDecimal().div(BD_18);
 }
